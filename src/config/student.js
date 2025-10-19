7882349a7ed47f13873d02ded7b85c6ed5d08535
insharaeman1 <insharaeman5@gmail.com>
2025-10-19 04:04:29 -0700

    Question1

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,4 @@
 // Edit these two fields with your details before you start.
 // They will be shown at the very top of the screen.
 export const STUDENT_NAME = "Inshara Eman Khadija";
-export const REG_NO = "Sp23-BSE-069";
+export const REG_NO = "Sp23-BSe-069";
